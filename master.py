e670dd0febe40548c5eeb85a0a77f38cb4de6e10
ghost_cfl <946930866@qq.com>
2020-02-28 15:17:51 +0800

    fix bug

--- master.py
+++ master.py
@@ -1,18 +1,22 @@
 import asyncio
 import mysql
 import re
+import os
+import shelve
 from settings import MY_TB_ACCOUNT, test_server
+from datetime import date
 from pyquery import PyQuery as pq
 
 test_server['db'] = 'test'
 
 
 class MasterSpider(object):
-    start_url = 'https://szsaibao.taobao.com/search.htm?search=y&orderType=hotsell_desc&pageNo='
+    start_url = 'https://shop.taobao.com/search.htm?search=y&orderType=hotsell_desc&pageNo='
     view_prot = {"width": 1600, "height": 900}
     total_page_num = 0
 
     def __init__(self, b, p):
+
         self.browser = b
         self.page = p
 
@@ -21,30 +25,40 @@ class MasterSpider(object):
         # self.loop.close()
         pass
 
-    async def get_html(self, page_control, speed=1):
+    async def _get_html(self, page_control, speed=1):
         page_num = 1
-        while page_num <= page_control:
-            await self.page.goto(self.start_url + str(page_num))
-            await self.page.waitForSelector(".item4line1")
-            page_num += 1
-            yield await self.page.content()
-            if self.total_page_num and page_num > self.total_page_num:
-                break
-            await asyncio.sleep(speed)
+        shop_infos = mysql.get_data(sql="select shop_id,typeabbrev from shop_info where shop_id!='88888888'",
+                                    dict_result=True)
+        for shop_info in shop_infos:
+            if shop_info["shop_id"] == "34933991":
+                url = self.start_url.replace("shop", "shop" + shop_info["shop_id"])
+            else:
+                continue
 
-    async def parse(self):
+            while page_num <= page_control:
+                await self.page.goto(url + str(page_num))
+                await self.page.waitForSelector(".item4line1")
+                page_num += 1
+                yield await self.page.content(), shop_info['shop_id'], shop_info['typeabbrev']
+                if self.total_page_num and page_num > self.total_page_num:
+                    break
+                await asyncio.sleep(speed)
+
+    async def _parse(self):
         item = {}
-        async for html in self.get_html(1000, speed=10):
+        async for html, ship_id, typeabbrev in self._get_html(1000, speed=10):
             doc = pq(html)
             if not self.total_page_num:
                 num = doc(".pagination span.page-info").text()
                 self.total_page_num = int(re.findall("/(\d+)", num)[0])
             items = doc(".item4line1 dl.item").items()
             for i in items:
-                item['typeabbrev'] = "TB"
-                item['shop_id'] = "34933991"
+                item['typeabbrev'] = typeabbrev
+                item['shop_id'] = ship_id
                 item['link_id'] = i.attr("data-id")
                 item['description'] = i.find("dd.detail a").text()
+                item['update_date'] = date.today()
+                item['flag'] = "insert"
                 if i.find("div.cprice-area span.c-price").text():
                     item['cprice'] = float(i.find("div.cprice-area span.c-price").text())
                 else:
@@ -60,22 +74,26 @@ class MasterSpider(object):
                 yield item
 
     async def save(self):
-        async for i in self.parse():
+        async for i in self._parse():
             res = mysql.get_data(db=test_server, t="tb_master", cn=['cprice', 'sprice', 'sale_num'],
                                  c={'link_id': i['link_id']}, dict_result=True)
             if res:
+                i['flag'] = "update"
                 if res[0]['cprice'] != i['cprice']:
-                    mysql.update_data(db=test_server, t="tb_master", set={"cprice": i['cprice']},
+                    update_dict = {
+                        "cprice": i['cprice'],
+                        "flag": "update_cprice",
+                        "old_cprice": res[0]['cprice'],
+                        "update_date": i["update_date"]
+                    }
+                    mysql.update_data(db=test_server, t="tb_master", set=update_dict,
                                       c={'link_id': i['link_id']})
-                    print("更新销售价")
                 if res[0]['sprice'] != i['sprice']:
                     mysql.update_data(db=test_server, t="tb_master", set={"sprice": i['sprice']},
                                       c={'link_id': i['link_id']})
-                    print("更新原价")
                 if res[0]['sale_num'] != i['sale_num']:
-                    mysql.update_data(db=test_server, t="tb_master", set={"sprice": i['sprice']},
+                    mysql.update_data(db=test_server, t="tb_master", set={"sale_num": i['sale_num']},
                                       c={'link_id': i['link_id']})
-                    print("更新销量")
             else:
                 mysql.insert_data(db=test_server, t="tb_master", d=i)
 
