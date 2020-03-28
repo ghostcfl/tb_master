f7f8b115a9a3d9d953fbe12d4d5bbd461893d95f
ghost_cfl <946930866@qq.com>
2020-03-28 08:17:43 +0800

    修复bug

--- master.py
+++ master.py
@@ -4,6 +4,7 @@ import time
 import re
 import Format
 import random
+import datetime
 from settings import test_server
 from pyquery import PyQuery
 from datetime import date
@@ -89,6 +90,10 @@ class Master(object):
             mysql.insert_data(db=test_server, t="tb_search_page_info", d=d)
             result = mysql.get_data(db=test_server, t="tb_search_page_info", c={"shop_id": shop_id}, dict_result=True)
 
+        if result[0]['last_date'] < datetime.date.today():
+            mysql.update_data(db=test_server, t="tb_search_page_info", set={"used_page_nums": "0"},
+                              c={"shop_id": shop_id})
+            result = mysql.get_data(db=test_server, t="tb_search_page_info", c={"shop_id": shop_id}, dict_result=True)
         #  获取已采集的数据的页码列表
         used_page_nums = [int(x) for x in result[0]['used_page_nums'].split(",")]
         total_page = result[0]['total_page']
@@ -139,9 +144,14 @@ class Master(object):
                 tspi = {  # tb_search_page_info
                     "used_page_nums": ",".join([str(x) for x in used_page_nums]),
                     "spent_time": spent_time,
+                    "last_date": datetime.date.today()
                 }
                 mysql.update_data(db=test_server, t="tb_search_page_info", set=tspi, c={"shop_id": shop_id})
                 page_num, used_page_nums, total_page = self._get_page_num(shop_id)
+            sql = "UPDATE tb_master SET flag='XiaJia',update_date='{}' WHERE shop_id='{}' AND update_date<'{}'".format(
+                datetime.date.today(), shop_id, datetime.date.today())
+            print(sql)
+            mysql.update_data(db=test_server, sql=sql)
 
     def _parse(self):
         for html, shop_id, curl, total_page in self._get_html():
@@ -201,7 +211,7 @@ class Master(object):
 
     def save(self):
         for i in self._parse():
-            print(i)
+            logger.info(i)
             res = mysql.get_data(db=test_server, t="tb_master",
                                  c={'link_id': i['link_id']}, dict_result=True)
             flag = ["update"]
@@ -216,6 +226,9 @@ class Master(object):
                 if res[0]['sale_num'] != i['sale_num']:
                     flag.append("sale")
                     narrative.append("更新销量:[{}]=>[{}]".format(res[0]['sale_num'], i['sale_num']))
+                if res[0]['flag'] == 'XiaJia':
+                    flag.append("ShangJia")
+                    narrative.append("下架商品重新上架")
                 i['flag'] = "_".join(flag)
                 i['narrative'] = ";".join(narrative)
                 mysql.update_data(db=test_server, t='tb_master', set=i, c={"link_id": i['link_id']})
@@ -227,73 +240,3 @@ class Master(object):
 if __name__ == '__main__':
     m = Master()
     m.save()
-
-# import requests
-# import re
-#
-# curl = 'curl "https://shop68559944.taobao.com/i/asynSearch.htm?_ksTS=1584414011939_808&callback=jsonp809&mid=w-16659046903-0&wid=16659046903&path=/search.htm&search=y&spm=a1z10.3-c-s.w4002-16659046903.31.199525e9yYUqQN&orderType=hotsell_desc&pageNo=4" -H "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:74.0) Gecko/20100101 Firefox/74.0" -H "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8" -H "Accept-Language: zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2" --compressed -H "Connection: keep-alive" -H "Cookie: enc=0G0Ym07eazGeaU4apn991R3TQsQbY2nDzl2wP1gVuYOMBDTkEesj5swMtywhQMgZFOrbH"%"2BKwWGBpdLI"%"2Bv3bcVQ"%"3D"%"3D; thw=cn; _m_h5_tk=522974ff5f7934c9f2ee66d99025b354_1584434156027; _m_h5_tk_enc=12a2812233ec7841e781c149b2c0d535; sgcookie=ENRpVB9vZ2"%"2FiPRnqfMJmD; tfstk=cJpNBQN7vAHZFYNkoTWVle6Vkz1OZzJkHySfsQcdYbvx095hiFaAxgslT_NxH1f..; pnm_cku822=098"%"23E1hvxpvUvbpvUpCkvvvvvjiPn2Fp1jYPP2dOQjYHPmPpQjiWP2sygjDPRLsyAjlPR4wCvvpvvUmm2QhvCPMMvvvCvpvVvmvvvhCvkphvC9QvvOCzpTyCvv9vvUvgi"%"2BD6RfyCvmFMMQCGS6vvgQvv9DCvpvo"%"2BvvmmZhCv2CUvvUEpphvWwpvv9D3vpvA1mphvLvseppvjOg2vqU0QKoZHtRFEDLuTWDAvD7zhV4tYVVzhAj7xhBwl"%"2BExrg8TJEcqhAj7gKL9"%"2BBaV9kUkZHkx"%"2FAjc6D46wjLPEDaexRfytvpvhvvvvvv"%"3D"%"3D; hibext_instdsigdipv2=1; t=2e904a7ddd36ecedf9084957aa2faee3; UM_distinctid=170ae018399245-0386396cd3d6e18-4c302879-13c680-170ae01839b1ae; mt=ci=0_0; isg=BMLCudMKKO3GWjSxI-B2iGa2EMgkk8atlr0VCwzb7jXgX2LZ9CMWvUg9C9sjFD5F; l=dBEhyV54Qyaozg02BOCCIFJ2en7OSIOYYuJNuLLvi_5CV6Y6O7_OorHC9Fv6VsWfT2LB4HrbH8p9-etkNQMmndhUOyjADxDc.; cna=hFCkFgfnYSQCAXFu01veEKo2; cookie2=1ad4f1a27d229546b21e420af6167a67; _tb_token_=e53e33193b1bf; v=0; hng=CN"%"7Czh-CN"%"7CCNY"%"7C156; _samesite_flag_=true; unb=2206498784722; sn=selingna5555"%"3Atest; uc1=cookie21=U"%"2BGCWk"%"2F7oPIg&cookie14=UoTUPvQVPDVwWg"%"3D"%"3D; csg=b1f8b152; _cc_=V32FPkk"%"2Fhw"%"3D"%"3D; skt=69e3bc85953b8f0b; ext_pgvwcount=2" -H "Upgrade-Insecure-Requests: 1" -H "TE: Trailers"'
-#
-#
-# def format_request_params(curl):
-#     curl = re.sub("\"", "", curl)
-#     a = curl.split(" -H ")
-#     params = {}
-#     cookies = {}
-#     headers = {}
-#     for b in a:
-#         # print(b)
-#         if re.match("curl", b):
-#             c = b.split(" ")[-1]
-#             url = c.split("?", 1)[0]
-#             d = c.split("?", 1)[1].split("&")
-#             for e in d:
-#                 f = e.split("=", 1)
-#                 params[f[0]] = f[1]
-#         elif re.match("Cookie", b):
-#             g = b.split(": ")[-1]
-#             h = g.split("; ")
-#             for i in h:
-#                 j = i.split("=", 1)
-#                 cookies[j[0]] = j[1]
-#         else:
-#             k = b.split(": ", 1)
-#             headers[k[0]] = k[1]
-#             # print(h)
-#     return url, params, cookies, headers
-#
-#
-# url, params, cookies, headers = format_request_params(curl)
-# # for k, v in cookies.items():
-# #     print(k + " " + v)
-# r = requests.get(url, headers=headers, params=params, cookies=cookies)
-# print(r.text)
-# import asyncio
-# from pyppeteer import launch
-# from settings import dev
-#
-# url = "https://szsaibao.taobao.com/search.htm?spm=a1z10.3-c-s.w4002-21808473997.28.35632d90dC9V0o&_ksTS=1584680372583_750&callback=jsonp751&input_charset=gbk&mid=w-21808473997-0&wid=21808473997&path=%2Fsearch.htm&search=y&orderType=hotsell_desc&pageNo=3"
-#
-#
-# async def run():
-#     b = await launch(dev)
-#     p = await b.newPage()
-#     await p.setViewport({"width": 1440, "height": 900})
-#     await p.goto(url)
-#     await p.waitForSelector(".shop-hesper-bd.grid",timeout=0)
-#     cookie = await p.cookies()
-#     print(cookie)
-#
-# if __name__ == '__main__':
-#     asyncio.run(run())
-# cookies = [{'name': 'l', 'value': 'dBTFO_L4Q1QLQLwEBOCg-FJ2wsbOSIRAguJNua3Hi_5QE681Fb_OozL09FJ6VjWftZYB4-9jaL29-etkZQDmndFM93Rw_xDc.', 'domain': '.taobao.com', 'path': '/', 'expires': 1600247232, 'size': 98, 'httpOnly': False, 'secure': False, 'session': False}, {'name': '_m_h5_tk_enc', 'value': '005d496ad4fc316097d34df926297952', 'domain': '.taobao.com', 'path': '/', 'expires': 1585300015.190792, 'size': 44, 'httpOnly': False, 'secure': False, 'session': False}, {'name': 'isg', 'value': 'BIyMWO--PqtlmCrXEaNgQEX2Xeq-xTBvHumx9-ZNmDfacSx7DtUA_4LDFXnJOWjH', 'domain': '.taobao.com', 'path': '/', 'expires': 1600247232, 'size': 67, 'httpOnly': False, 'secure': False, 'session': False}, {'name': 'uc1', 'value': 'cookie14=UoTUPvbEMOMRWQ%3D%3D&lng=zh_CN', 'domain': '.taobao.com', 'path': '/', 'expires': -1, 'size': 42, 'httpOnly': False, 'secure': False, 'session': True}, {'name': 'tfstk', 'value': 'ctlhBnfx8vyBDnBPh9NIPZaFDbRAZB7av0oI_XjTnQP0CJcNiX6N3Db9Ilq5_T1..', 'domain': '.taobao.com', 'path': '/', 'expires': 1600247212, 'size': 70, 'httpOnly': False, 'secure': False, 'session': False}, {'name': 'v', 'value': '0', 'domain': '.taobao.com', 'path': '/', 'expires': -1, 'size': 2, 'httpOnly': False, 'secure': False, 'session': True}, {'name': 'cna', 'value': 'nXX7FveVhAcCAXFusEjwsp5o', 'domain': '.taobao.com', 'path': '/', 'expires': 2215415211, 'size': 27, 'httpOnly': False, 'secure': False, 'session': False}, {'name': 'skt', 'value': 'c62b0734f3bad2dc', 'domain': '.taobao.com', 'path': '/', 'expires': -1, 'size': 19, 'httpOnly': True, 'secure': False, 'session': True}, {'name': 'sn', 'value': 'simpleli%3A%E6%A2%81%E7%A5%A5%E5%81%A5', 'domain': '.taobao.com', 'path': '/', 'expires': -1, 'size': 40, 'httpOnly': False, 'secure': False, 'session': True}, {'name': '_tb_token_', 'value': 'e881854fe5e39', 'domain': '.taobao.com', 'path': '/', 'expires': -1, 'size': 23, 'httpOnly': False, 'secure': False, 'session': True}, {'name': 'unb', 'value': '2204928296721', 'domain': '.taobao.com', 'path': '/', 'expires': -1, 'size': 16, 'httpOnly': True, 'secure': False, 'session': True}, {'name': 'sgcookie', 'value': 'EkVfHPG1Nf7UM9LF%2F5I8N', 'domain': '.taobao.com', 'path': '/', 'expires': 1616231211.832112, 'size': 31, 'httpOnly': True, 'secure': False, 'session': False}, {'name': 'cookie2', 'value': '1fe82b1f3376d91898c5b3bcab61370c', 'domain': '.taobao.com', 'path': '/', 'expires': -1, 'size': 39, 'httpOnly': True, 'secure': False, 'session': True}, {'name': 'x', 'value': '31295113', 'domain': '.taobao.com', 'path': '/', 'expires': -1, 'size': 9, 'httpOnly': False, 'secure': False, 'session': True}, {'name': '_samesite_flag_', 'value': 'true', 'domain': '.taobao.com', 'path': '/', 'expires': -1, 'size': 19, 'httpOnly': True, 'secure': True, 'session': True}, {'name': 'csg', 'value': 'df54c6df', 'domain': '.taobao.com', 'path': '/', 'expires': -1, 'size': 11, 'httpOnly': False, 'secure': False, 'session': True}, {'name': '_m_h5_tk', 'value': 'cf154e8be6d7732ec0a1372eb198f3f0_1584702775081', 'domain': '.taobao.com', 'path': '/', 'expires': 1585300015.190717, 'size': 54, 'httpOnly': False, 'secure': False, 'session': False}, {'name': 't', 'value': '4e92cac1d1650b21ad01669d69c54b5f', 'domain': '.taobao.com', 'path': '/', 'expires': 1592471198.804966, 'size': 33, 'httpOnly': False, 'secure': False, 'session': False}, {'name': 'pnm_cku822', 'value': '', 'domain': 'szsaibao.taobao.com', 'path': '/', 'expires': 1587287230, 'size': 10, 'httpOnly': False, 'secure': False, 'session': False}, {'name': 'thw', 'value': 'cn', 'domain': '.taobao.com', 'path': '/', 'expires': 1616231194.192734, 'size': 5, 'httpOnly': False, 'secure': False, 'session': False}]
-# cookie = {}
-# for x in cookies:
-#     if x['name'] != 'sn':
-#         cookie[x['name']] = x['value']
-#
-# import requests
-# headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:74.0) Gecko/20100101 Firefox/74.0', 'Accept': 'text/javascript, application/javascript, application/ecmascript, application/x-ecmascript, */*; q=0.01', 'Accept-Language': 'zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2 --compressed', 'Referer': 'https://shop115443253.taobao.com/search.htm?_ksTS=1584682864154_932&callback=jsonp933&mid=w-9539895920-0&wid=9539895920&path=/search.htm&search=y&orderType=hotsell_desc&pageNo=40', 'X-Requested-With': 'XMLHttpRequest', 'Connection': 'keep-alive', 'TE': 'Trailers'}
-# print(cookie)
-# r = requests.get(url="https://szsaibao.taobao.com/i/asynSearch.htm?mid=w-21808473997-0&path=/search.htm&search=y&input_charset=gbk&orderType=hotsell_desc&pageNo=3", headers=headers, cookies=cookie)
-# print(r.text)
-# # _ksTS=1584692676219_750&callback=jsonp751
--- reports.py
+++ reports.py
@@ -1,5 +1,5 @@
 import mysql
-import Format
+import datetime
 from settings import test_server
 from smtp import mail
 from settings import my_user
@@ -8,9 +8,16 @@ test_server['db'] = 'test'
 
 
 class Reports(object):
+    translate_dictionary = {
+        "insert": "新增",
+        "price": "更新销售价格",
+        "promotion": "更新优惠价格",
+        "sale": "更新销量",
+        "XiaJia": "下架商品",
+        "ShangJia": "重新上架商品",
+    }
 
-    def get(self, shop_ids, flags):
-
+    def get(self, shop_ids):
         shop_report_groups = []
         for shop_id in shop_ids:
             shop_name = mysql.get_data(
@@ -18,17 +25,26 @@ class Reports(object):
                 return_one=True
             )
             flag_report_groups = []
-            for flag in flags:
+            # sql = "SELECT COUNT(id) AS nums FROM tb_master where shop_id='{}' and update_date<'{}'".format(shop_id,
+            #                                                                                                datetime.date.today())
+            # nums = mysql.get_data(db=test_server, sql=sql, return_one=True)
+            # if int(nums) > 0:
+            #     flag_report_groups.append("下架商品{}条".format(nums))
+            # del sql
+            # del nums
+            for flag in self.translate_dictionary.keys():
                 sql = "SELECT COUNT(id) AS nums FROM tb_master WHERE flag LIKE '%%{}%%' AND shop_id='{}'".format(flag,
                                                                                                                  shop_id)
                 nums = mysql.get_data(db=test_server, sql=sql, return_one=True)
                 if int(nums) > 0:
-                    flag_report_groups.append("{}{}条".format(self._flag_translation(flag), nums))
+                    flag_report_groups.append("{}{}条".format(self.translate_dictionary[flag], nums))
+
             if flag_report_groups:
-                page_num = Format._read(shop_id, "page_num")
-                flag_report_groups.append("总计爬取{}页".format(page_num))
-                spent_time = Format._read(shop_id, "spent_time")
-                flag_report_groups.append("总计花费{}分{}秒".format(int(spent_time / 60), int(spent_time % 60)))
+                result = mysql.get_data(db=test_server, t="tb_search_page_info",
+                                        c={"shop_id": shop_id}, dict_result=True)
+                flag_report_groups.append("总计爬取{}页".format(result[0]['total_page']))
+                flag_report_groups.append(
+                    "总计花费{}分{}秒".format(int(result[0]['spent_time'] / 60), int(result[0]['spent_time'] % 60)))
                 flag_report_groups.reverse()
                 flag_report_groups.append(shop_name)
                 flag_report_groups.reverse()
@@ -36,22 +52,14 @@ class Reports(object):
 
         return "\n".join(shop_report_groups)
 
-    @staticmethod
-    def _flag_translation(flag):
-        translate_dictionary = {
-            "insert": "新增",
-            "price": "更新销售价格",
-            "promotion": "更新优惠价格",
-            "sale": "更新销量",
-        }
-        return translate_dictionary[flag]
-
     def report(self, shop_ids):
-        flags = ["insert", "price", "promotion", "sale"]
-        r = self.get(shop_ids=shop_ids, flags=flags)
+        r = self.get(shop_ids=shop_ids)
+        print(r)
         mail("店铺搜索页爬虫报告", r, my_user)
-        [Format._write(shop_id=shop_id, flag="mail", value=1) for shop_id in shop_ids]
+        # [Format._write(shop_id=shop_id, flag="mail", value=1) for shop_id in shop_ids]
 
 
 if __name__ == '__main__':
-    pass
+    report = Reports()
+    ids = ["115443253", "33817767", "34933991", "131282813", "68559944", "60299985", "197444037"]
+    report.report(ids)
